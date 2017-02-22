ac74212a9c9d6cd80dea7cf9d8db836ac5276471
twosunny <singwoosun@gmail.com>
2017-02-22 15:02:12 +0900

    mongodb CRUD

--- app/controller/user.server.controller.js
+++ app/controller/user.server.controller.js
@@ -23,3 +23,42 @@ exports.list = function(req,res,next){
         }
     })
 }
+
+exports.userByID = function(req,res,next,id){
+    User.findOne({
+        _id : id
+    }, function(err,user){
+        if(err){
+            return next();
+        }else{
+            req.user = user;
+            next();
+        }
+    });
+
+}
+
+exports.read = function(req,res){
+    res.json(req.user);
+}
+
+exports.update = function(req,res,next) {
+    User.findByIdAndUpdate(req.user.id, req.body, function(err, user) {
+        if(err) {
+            return next(err);
+        }else{
+            res.json(user);
+        }
+    });
+};
+
+exports.delete = function(req,res,next) {
+    req.user.remove(function(err) {
+        if(err) {
+            return next(err);
+        }else{
+            res.json(req.user);
+        }
+    })
+};
+
--- app/route/user.server.route.js
+++ app/route/user.server.route.js
@@ -5,4 +5,13 @@ module.exports = function(app) {
     .post(users.create)
     .get(users.list);
 
+    app.route('/users/:userId')
+    .get(users.read)
+    .put(users.update)
+    .delete(users.delete);
+
+    app.param('userId', users.userByID);
+
+    
+
 };
